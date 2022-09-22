efdca9775dd50a10226f5ffbd7477bb1e4478844
Bran <jiakuo.zhang@outlook.com>
2022-09-22 11:14:29 +0800

    ud

--- Malfurion.WebApi/Global.cs
+++ Malfurion.WebApi/Global.cs
@@ -1,6 +1,8 @@
 global using System;
 global using System.Net;
 global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.DependencyInjection;
 global using Newtonsoft.Json;
 
 global using Malfurion.WebApi.Dtos;
---
+++ Malfurion.WebApi/MalfurionWebApiExtensions.cs
@@ -0,0 +1,18 @@
+namespace Malfurion.WebApi;
+using Malfurion.WebApi.Services;
+using Malfurion.WebApi.Middlewares;
+public static class MalfurionWebApiExtensions
+{
+    public static IServiceCollection AddMalfurionWebApi(this IServiceCollection services)
+    {
+        services.AddScoped<ResponseService>();
+        return services;
+    }
+
+    public static IApplicationBuilder UseMalfurionWebApi(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<HttpCodeMiddleware>();
+        app.UseMiddleware<ExceptionMiddleware>();
+        return app;
+    }
+}
