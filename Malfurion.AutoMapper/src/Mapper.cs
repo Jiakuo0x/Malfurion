3382b4ce8fdb2d5d957168b9b10cdde1fa1f88a5
Bran <jiakuo.zhang@outlook.com>
2022-10-17 17:41:22 +0800

    ud

---
+++ Malfurion.AutoMapper/src/Mapper.cs
@@ -0,0 +1,52 @@
+﻿namespace Malfurion.AutoMapper;
+public static class Mapper
+{
+    public static RTarget Map<RTarget>(object source)
+        where RTarget : new()
+    {
+        RTarget result = new();
+
+        if (source is null)
+            return result;
+
+        Map(source, result);
+
+        return result;
+    }
+
+    public static void Map(object source, object target)
+    {
+        if (source is null)
+        {
+            target = null!;
+            return;
+        }
+        if (target is null)
+            target = new();
+
+        var sourceProperties = source.GetType().GetProperties();
+        foreach (var sourceProperty in sourceProperties)
+        {
+            var targetProperty = target.GetType().GetProperty(sourceProperty.Name);
+            if (targetProperty is null)
+            {
+                continue;
+            }
+            if (sourceProperty.PropertyType.IsValueType)
+            {
+                var sourceValue = sourceProperty.GetValue(source);
+                targetProperty.SetValue(target, sourceValue);
+            }
+            else if (sourceProperty.PropertyType.IsClass)
+            {
+                var sourceValue = sourceProperty.GetValue(source);
+                var targetValue = targetProperty.GetValue(target);
+                if(targetValue is null)
+                {
+                    targetValue = Activator.CreateInstance(targetProperty.PropertyType);
+                }
+                Map(sourceValue!, targetValue!);
+            }
+        }
+    }
+}
--- Malfurion.WebApi/Global.cs
+++
@@ -1,9 +0,0 @@
-global using System;
-global using System.Net;
-global using Microsoft.AspNetCore.Http;
-global using Microsoft.AspNetCore.Builder;
-global using Microsoft.Extensions.DependencyInjection;
-global using Newtonsoft.Json;
-
-global using Malfurion.WebApi.Dtos;
-global using Malfurion.WebApi.Services;
